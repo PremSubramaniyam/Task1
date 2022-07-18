d2e17f5fc5d5262d94ab40efc7da4cb651ce79b1
PremSubramaniyam <102665183+PremSubramaniyam@users.noreply.github.com>
2022-07-18 16:15:02 +0530

    Add files via upload

--- Task/src/com/cg/task/MainClass.java
+++ Task/src/com/cg/task/MainClass.java
@@ -2,31 +2,47 @@ package com.cg.task;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class MainClass {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		List<Employee> list = new ArrayList<Employee>();
-		
+
 		list.add(new Employee("Ajith", 30));
 		list.add(new Employee("vikas", 24));
 		list.add(new Employee("Johnson", 19));
 		list.add(new Employee("Victor", 23));
-		
+
 		List<Employee> list3 = list.stream()
-						.filter(e -> e.getAge() < 24)
-						.collect(Collectors.toList());
-		
-		list3.forEach( e -> System.out.println("Name: " + e.getName() + " Age: " + e.getAge()));
+									.filter(e -> e.getAge() < 24)
+									.collect(Collectors.toList());
+
+		list3.forEach(e -> System.out.println("Name: " + e.getName() + " Age: " + e.getAge()));
+
+		System.out.println("Method Reference Example ->");
+		System.out.println("Sort by name :");
+
+		list.stream()
+			.map(x -> x.getName())
+			.forEach(System.out::println); 
+
+		System.out.println("Sort by age :");
+
+		list.stream()
+			.map(x -> x.getAge() < 24)
+			.forEach(System.out::println);
 		
 		/*
-		 * for(Employee e : list) { if(e.getAge() < 24) { list3.add(e); } }
+		 * Optional<Integer> op = Optional.ofNullable(1235);
 		 * 
-		 * for(Employee e : list) { System.out.println("Name: " + e.getName() +
-		 * " , Age: " + e.getAge()); }
-		 */		
+		 * Optional<Integer> value = op.filter(number -> number % 2 == 0);
+		 * 
+		 * if (value.isEmpty()) { System.out.println("Odd number"); } else {
+		 * System.out.println("Even number"); }
+		 */
+        
 	}
 
 }
